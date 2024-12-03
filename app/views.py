c64771b2089924a46a4be02506690836ac23f600
Lakshmipriyaloki <Lakshmipriyaloki@gmail.com>
2024-12-03 17:43:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Sagar(request):
+    return HttpResponse('Brother from another mother')
+def Anil(request):
+    return HttpResponse('call me senior')
